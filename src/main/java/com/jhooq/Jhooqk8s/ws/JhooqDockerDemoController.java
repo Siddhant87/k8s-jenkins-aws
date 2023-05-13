2417a92621ba81436771b2f12449a79142453d93
Siddhant87 <siddhantkumar87@gmail.com>
2023-05-14 03:24:32 +0530

    commit

--- src/main/java/com/jhooq/Jhooqk8s/ws/JhooqDockerDemoController.java
+++ src/main/java/com/jhooq/Jhooqk8s/ws/JhooqDockerDemoController.java
@@ -8,6 +8,6 @@ public class JhooqDockerDemoController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello - s-k8s i Have updated the message";
+        return "Hello - sid-k8s i Have updated the message";
     }
 }
