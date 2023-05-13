607940ac6ccedd7475e31fdd19317993c941274e
Siddhant87 <60612524+Siddhant87@users.noreply.github.com>
2023-05-14 04:25:19 +0530

    Update JhooqDockerDemoController.java

--- src/main/java/com/jhooq/Jhooqk8s/ws/JhooqDockerDemoController.java
+++ src/main/java/com/jhooq/Jhooqk8s/ws/JhooqDockerDemoController.java
@@ -8,6 +8,6 @@ public class JhooqDockerDemoController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello - aaaa-k8s i Have updated the message";
+        return "Hello - aa-k8s i Have updated the message";
     }
 }
